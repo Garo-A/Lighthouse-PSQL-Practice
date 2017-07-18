0fe7ab916c2c7f54a6e80cf3e2547e2ffd16839b
Garo-A <garo.assilian@gmail.com>
2017-07-18 21:12:23 +0000

    Modified usingKnex.js to accomadate multiple people having same name.

--- usingKnex.js
+++ usingKnex.js
@@ -20,11 +20,11 @@ function getString(obj) {
   return string;
 }
 
-//Responsible for printing out everything.
-function printOutput(rowcount, output){
-  console.log(`Found ${rowcount} person(s) with name ${name}`);
-  console.log(output);
-}
+// //Responsible for printing out everything.
+// function printOutput(rowcount, output){
+//   console.log(`Found ${rowcount} person(s) with name ${name}`);
+//   console.log(output);
+// }
 
 
 knex.select('*').from('famous_people').where('first_name', name).orWhere('last_name',name)
@@ -33,6 +33,9 @@ knex.select('*').from('famous_people').where('first_name', name).orWhere('last_n
     console.log('Cannot get data');
     return;
   }
-  printOutput(rows.length, getString(rows[0]));
+  console.log(`Found ${rows.length} person(s) with name ${name}`)
+  for (let i = 0; i < rows.length; i++){
+    console.log(getString(rows[i]));
+  }
 })
 knex.destroy();
