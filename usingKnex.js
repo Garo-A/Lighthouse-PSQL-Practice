b29bd2c47ca27942035f6f4af76b9dac3cf67c1f
Garo-A <garo.assilian@gmail.com>
2017-07-18 18:00:01 +0000

    Added knex version of lookup.js

---
+++ usingKnex.js
@@ -0,0 +1,37 @@
+const settings = require ('./settings');
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host :      settings.hostname,
+    user :      settings.user,
+    password :  settings.password,
+    database :  settings.database
+  }
+});
+
+const args = process.argv.slice(2);
+const name = args[0];
+console.log('name');
+
+
+function getString(obj) {
+
+  let string = `- ${obj.id}: ${obj.first_name} ${obj.last_name}, born ${obj.birthdate}`
+  return string;
+}
+
+//Responsible for printing out everything.
+function printOutput(rowcount, output){
+  console.log(`Found ${rowcount} person(s) with name ${name}`);
+  console.log(output);
+}
+
+
+knex.select('*').from('famous_people').where('first_name', name).orWhere('last_name',name).asCallback(function (err, rows){
+  if (err) {
+    console.log('Cannot get data');
+    return;
+  }
+  printOutput(rows.length, getString(rows[0]));
+})
+knex.destroy();
